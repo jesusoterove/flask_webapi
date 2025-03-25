a8617997e07c28b42b6b47c1a3f224d41a2b6de8
Jesus Otero <jesusoterove@gmail.com>
2025-03-25 16:29:52 -0500

    Modified requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+requirements = [
+    'flask==3.1.0',
+]
+
 setup(
     name='Flask-WebApi',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=[
-        'flask==3.1.0',
-    ],
+    install_requires=requirements,
     author='Jesus Otero',
     author_email='jesusoterove@gmail.com',
     description='Python Webapi for flask done dotnet style',
-    url='https://github.com/jesusoterove/Flask-WebApi',
+    url='https://github.com/jesusoterove/flask_webapi',
+    project_urls={
+        'Source': 'https://github.com/jesusoterove/flask_webapi',
+    },
 )
