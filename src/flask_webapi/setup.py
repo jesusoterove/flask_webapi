e8673506bb38066796da3a6a0d9f7738af7f71c0
Jesus Otero <jesusoterove@gmail.com>
2025-03-25 16:05:16 -0500

    Project initialized

---
+++ src/flask_webapi/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-WebApi',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'flask==3.1.0',
+    ],
+    author='Jesus Otero',
+    author_email='jesusoterove@gmail.com',
+    description='Python Webapi for flask done dotnet style',
+    url='https://github.com/jesusoterove/Flask-WebApi',
+)
