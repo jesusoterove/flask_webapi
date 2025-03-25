49c696bebf766110cec416adfb08180641af23ee
Jesus Otero <jesusoterove@gmail.com>
2025-03-25 16:27:10 -0500

    Folder structure changed

--- src/flask_webapi/exceptions/api_exception.py
+++
@@ -1,5 +0,0 @@
-class ApiException(Exception):
-    def __init__(self, message, status_code):
-        self.message = message
-        self.status_code = status_code
-        super(ApiException, self).__init__(message)
